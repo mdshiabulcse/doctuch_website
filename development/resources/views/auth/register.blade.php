9847cd0e0865c606cd856d39c3a4ef342b3a3c67
md shiabul islam <md.shiabul.cse@gmail.com>
2024-02-05 12:47:57 +0600

    asset and html to laravel convert pending by development

--- development/resources/views/auth/register.blade.php
+++ development/resources/views/auth/register.blade.php
@@ -6,59 +6,72 @@
             <div class="position-relative z-index-5">
                 <div class="row">
                     <div class="col-xl-7 col-xxl-8">
-                        <a href="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/html/index.html" class="text-nowrap logo-img d-block px-4 py-9 w-100">
-                            <img src="{{asset('admin/')}}/assets/images/logos/dark-logo.svg" class="dark-logo" alt="Logo-Dark" />
-                            <img src="{{asset('admin/')}}/assets/images/logos/light-logo.svg" class="light-logo" alt="Logo-light" />
+                        <a href="https://doctuch.com"
+                           class="text-nowrap logo-img d-block px-4 py-9 w-100">
+                            <img src="{{asset('admin/')}}/assets/images/logos/dark-logo.svg" class="dark-logo"
+                                 alt="Logo-Dark"/>
+                            <img src="{{asset('admin/')}}/assets/images/logos/light-logo.svg" class="light-logo"
+                                 alt="Logo-light"/>
                         </a>
-                        <div class="d-none d-xl-flex align-items-center justify-content-center" style="height: calc(100vh - 80px);">
-                            <img src="{{asset('admin/')}}/assets/images/backgrounds/login-security.svg" alt="" class="img-fluid" width="500">
+                        <div class="d-none d-xl-flex align-items-center justify-content-center"
+                             style="height: calc(100vh - 80px);">
+                            <img src="{{asset('admin/')}}/assets/images/backgrounds/login-security.svg" alt=""
+                                 class="img-fluid" width="500">
                         </div>
                     </div>
                     <div class="col-xl-5 col-xxl-4">
-                        <div class="authentication-login min-vh-100 bg-body row justify-content-center align-items-center p-4">
+                        <div
+                            class="authentication-login min-vh-100 bg-body row justify-content-center align-items-center p-4">
                             <div class="col-sm-8 col-md-6 col-xl-9">
-                                <h2 class="mb-3 fs-7 fw-bolder">Welcome to Modernize</h2>
+                                <h2 class="mb-3 fs-7 fw-bolder">Welcome to DOCTUCH</h2>
                                 <p class=" mb-9">Your Admin Dashboard</p>
-                                <div class="row">
-                                    <div class="col-6 mb-2 mb-sm-0">
-                                        <a class="btn btn-white text-dark border fw-normal d-flex align-items-center justify-content-center rounded-2 py-8"
-                                           href="javascript:void(0)" role="button">
-                                            <img src="{{asset('admin/')}}/assets/images/svgs/google-icon.svg" alt="" class="img-fluid me-2" width="18"
-                                                 height="18">
-                                            <span class="d-none d-sm-block me-1 flex-shrink-0">Sign in with</span>Google
-                                        </a>
-                                    </div>
-                                    <div class="col-6">
-                                        <a class="btn btn-white text-dark border fw-normal d-flex align-items-center justify-content-center rounded-2 py-8"
-                                           href="javascript:void(0)" role="button">
-                                            <img src="{{asset('admin/')}}/assets/images/svgs/facebook-icon.svg" alt="" class="img-fluid me-2" width="18"
-                                                 height="18">
-                                            <span class="d-none d-sm-block me-1 flex-shrink-0">Sign in with</span>FB
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="position-relative text-center my-4">
-                                    <p class="mb-0 fs-4 px-3 d-inline-block text-bg-white text-dark z-index-5 position-relative">or sign Up
-                                        with</p>
-                                    <span class="border-top w-100 position-absolute top-50 start-50 translate-middle"></span>
-                                </div>
-                                <form>
+                                <form method="POST" action="{{ route('register') }}">
+                                    @csrf
                                     <div class="mb-3">
-                                        <label for="exampleInputEmail1" class="form-label">Name</label>
-                                        <input type="text" class="form-control" id="exampleInputtext" aria-describedby="textHelp">
+                                        <label for="exampleInputtext" class="form-label">Name</label>
+                                        <input id="exampleInputtext" aria-describedby="textHelp" type="text"
+                                               class="form-control @error('name') is-invalid @enderror" name="name"
+                                               value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                        @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
                                     </div>
                                     <div class="mb-3">
                                         <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        <input id="exampleInputEmail1" type="email"
+                                               class="form-control @error('email') is-invalid @enderror" name="email"
+                                               value="{{ old('email') }}" required autocomplete="email"
+                                               aria-describedby="emailHelp">
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
                                     </div>
                                     <div class="mb-4">
                                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1">
+                                        <input type="password"
+                                               class="form-control @error('password') is-invalid @enderror"
+                                               name="password" required autocomplete="new-password"
+                                               id="exampleInputPassword1">
+                                        @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
                                     </div>
-                                    <a href="authentication-login.html" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Sign Up</a>
+                                    <div class="mb-4">
+                                        <label for="password-confirm" class="form-label">Confirm Password</label>
+                                        <input id="password-confirm" type="password" class="form-control"
+                                               name="password_confirmation" required autocomplete="new-password">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary w-100 py-8 mb-4 rounded-2">Sign Up
+                                    </button>
                                     <div class="d-flex align-items-center">
                                         <p class="fs-4 mb-0 text-dark">Already have an Account?</p>
-                                        <a class="text-primary fw-medium ms-2" href="authentication-login.html">Sign In</a>
+                                        <a class="text-primary fw-medium ms-2" href="{{route('login')}}">Sign In</a>
                                     </div>
                                 </form>
                             </div>
